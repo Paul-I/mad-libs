831f52fff67cb3e11196d4c602b3f7416d321370
Paul Ijagbemi <paulijagbemi@seoulchristian.org>
2016-01-22 23:26:06 +0900

    madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+story = """
+We went to {} and we play {}.
+After we played we ate {}  and we slept for {} hours.
+After we woke up we ate {} and got dress to go to {}. """
+
+def main():
+#This what allows the user to input the information required
+    location = raw_input("Enter a name of a place: ")
+    game = raw_input("Enter a name of a game: ")
+    food = raw_input("Enter a name of a food: ")
+    hours = raw_input("Enter a number of hours: ")
+    food = raw_input("Enter a name of a food again: ")
+    place = raw_input("Enter a name of a place again: ")
+#This is what tells the program how to format the words in order
+    mad_lib = story.format(location,
+                            game,
+                            food[0],
+                            hours,
+                            food[1],
+                            place,)
+
+#This is what print the story after it made
+    print(mad_lib)
+
+main()
